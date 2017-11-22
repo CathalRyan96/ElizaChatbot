377f54f7b2dff12143a1866be9e54740b021aff8
CathalRyan96 <cathalryan48@gmail.com>
2017-11-22 13:44:00 +0000

    added elizaHandler

--- elizaMain.go
+++ elizaMain.go
@@ -2,12 +2,34 @@ package main
 
 import(
 	"net/http"
-	
+	"html/template"
 )
 
+type myMsg struct{
+	ER string
+	UR string
+}
+
+func requestHandler(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "text/html")
+
+	
+}
+
+func eliza(w http.ResponseWriter, r *http.Request){
+	elizaResponse := "Hi, how are you today?"
+
+	userResponse := r.FormValue("userResponse")
+
+	t,_ := template.ParseFiles("chatbot.html")
+
+	t.Execute(w, &myMsg{ER:elizaResponse, UR:userResponse})
+}
 
 func main(){
 	http.Handle("/", http.FileServer(http.Dir("./static")))
+
+	http.HandleFunc("/ElizaChatbot",eliza)
 
 	http.ListenAndServe(":8080", nil)
 }
